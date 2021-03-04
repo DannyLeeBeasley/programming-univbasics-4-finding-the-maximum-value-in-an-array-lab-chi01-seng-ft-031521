720d0fe13b64dfab4e1fa9f7fe607794391e82ef
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-04 21:22:20 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  
+  count = 0
+  while count <= array.length do
+    if array[count] > 
+      max_value = count
+    end
+    count += 1
+  end
+  max_value
+  end
 end
